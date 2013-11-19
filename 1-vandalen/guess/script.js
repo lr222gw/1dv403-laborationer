807c9fc0d6fd2fcb36623032870dac3dbd24dae9
lr222gw <lr222gw@student.lnu.se>
2013-11-19 21:11:14 +0100

    Labb 1 klar
    Labb 1 är klar, men jag har ett problem och det är:
    när användaren matar in bokstäver/text så blir det inget error
    meddelande eller dylikt..

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,16 +2,34 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
-	
+    var secret = Math.floor(Math.random() * 100 + 1); //innebär att det finn 100 möjliga värden, och man börjar från 1 (alltså 1 till 100..)
+    
+	var count = 0;
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
-		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
-		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
+	    console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
+	    console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
+	    count += 1;	    
+	    
+	    //if (typeof number !== typeof 10) {  // DET HÄR FUNGERAR INTE! testat allt. men inget fungerar..
+	    //    alert(typeof number);
+	    //    return [false, "Du måste ange Siffror!"];
+	    //};
 
+	    if (number > 100 || number < 0) {
+	        return [false, "Talet är utanför intervallet 0 - 100"];
+	    };
+	    if (number == secret) { // Fungerar inte att skriva "number === secret" här?? måste vara "==" ... hmm
 
+	        return [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + count+ " gissningar för att hitta det."];
+	    };
+	    if(number < secret){
+	        return [false, "Det hemliga talet är högre!"]
+	    };
+	    if (number > secret) {
+	        return [false, "Det hemliga talet är lägre!"]
+	    };
+	    
 		// Returnera exempelvis: 
 		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
 		// [false, "Det hemliga talet är högre!"]
