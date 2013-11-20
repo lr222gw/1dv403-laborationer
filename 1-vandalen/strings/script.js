1bfce165b0fcb7c71e02dc999d30d3c51b8eb70f
lr222gw <lr222gw@student.lnu.se>
2013-11-20 17:05:59 +0100

    Labb 1.2 KLAR!

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -8,7 +8,7 @@ window.onload = function(){
 		// Returnera den konverterade strängen.
 		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
 	
-	    var to = str; 
+	    var to = str; //obs detta gjorde jag bara så att jag kunde testa.. hade lika gärna kunnat skriva "str" direkt i for-loopen..
 	    var i = 0;
 	    var count = 0; 
 	    var output = " "; // så att det blir en sträng
@@ -16,12 +16,21 @@ window.onload = function(){
 	    for (var i = 0; i < to.length ; i++) // för varje bokstav i variabeln "to" <-- ändras väll så småning om, testar bara nu..
 	    {
 	        var newChar = to[i].toUpperCase(); // to[i] <-- markerar den bokstav jag vill ändra.. (i berättar vilken bokstav det är..)
+                                                // newChar Får in bokstaven från to[i]  fast som stor. i if-satsen testas det om newChar och to[i] är samma, (det ÄR samma bokstav) det som testar är om båda bokstäverna är STORA..
+                                                //                                  ^-i står för vilken bokstav i strängen to
+	        var the_aChar = 'a';
+	        var the_AChar = 'A';
+	        if (to[i] === the_aChar || to[i] === the_AChar)
+	        {
+	            output += "#";
+	            continue; // bryter loopen och börjar nu om 
+	        }
 	        if(newChar === to[i])//to.charAt(i).toUpperCase(to.charAt(i))
 	        {
-	            output += newChar.toLowerCase();
+	            output += newChar.toLowerCase(); // Här konkateneras (stavas inte så.. ) output ihop med newChar.toLoweCase()
 
 	            //to.charAt(i).replace(to.charAt(i), to.toLowerCase(to.charAt(i)));
-	        }else 
+	        }else // om inte if-satsen "fungerar" så innebär det att denna sats ska göras. Det innebär alltså att om denna sats utförs så är det för att bokstaven inte var stor och då är den ju liten <-- den ska bli stor! (det händer i denna sats..)
 	        {
 	            output += newChar.toUpperCase();
 	        }
