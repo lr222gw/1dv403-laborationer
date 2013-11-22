0e56217ca3eaedc048708012baee1cd3bec1bea6
lr222gw <lr222gw@student.lnu.se>
2013-11-22 16:13:01 +0100

    Labb 1.4 snart klar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -4,17 +4,39 @@ var makePerson = function(persArr){
 
     // Ta persArr bryta upp den i flera delar(Name, age). Delarna ska sedan samlas i ett objekt med arrayer. Objectets arrayer ska sedan behandlas (tex ange min,max och average). objectet ska returneras tillbaka
     var i;
-    var objectArr = {name: [], age : []};
+    var objectArr = { names: "Null", minAge: 0, maxAge: 0, averageAge: 0 };
+    var differentAges = [];
+    var differentNames = [];
+    var findMax = 0;
+    var findMin = Infinity - 1;
+    var allAgeTogheter = 0;
+
 
     for(i = 0; i < persArr.length ; i++) // ska hålla på tills det inte finns mer object i arrayen...
-    {
-        
-        objectArr.name.push(persArr[i].name);
-        objectArr.age.push(persArr[i].age);
-        
+    {        
+        differentNames.push(persArr[i].name);
+        differentAges.push(persArr[i].age);
+               
     }
 
-    
+    for (i = 0; i < differentAges.length; i++)
+    {
+        if(differentAges[i] > findMax)
+        {
+            findMax = differentAges[i];
+        }
+        if (differentAges[i] < findMin) {
+            findMin = differentAges[i];
+        }
+        allAgeTogheter += differentAges[i];
+
+    }
+
+    objectArr.maxAge = findMax;
+    objectArr.minAge = findMin;
+    objectArr.averageAge = (allAgeTogheter) / differentAges.length;
+    differentNames.sort(); // sorterar till bokstavsordning
+    objectArr.names = differentNames.join(", ");
 
     return objectArr;
 }
