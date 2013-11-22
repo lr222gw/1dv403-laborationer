805793ce80caa86aae698c5ee6e24973fa066630
lr222gw <lr222gw@student.lnu.se>
2013-11-22 14:39:55 +0100

    kommit en bit på labb 1.4

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,24 @@
 
 var makePerson = function(persArr){
 
+    // Ta persArr bryta upp den i flera delar(Name, age). Delarna ska sedan samlas i ett objekt med arrayer. Objectets arrayer ska sedan behandlas (tex ange min,max och average). objectet ska returneras tillbaka
+    var i;
+    var objectArr = {name: [], age : []};
 
-	// Din kod här...
+    for(i = 0; i < persArr.length ; i++) // ska hålla på tills det inte finns mer object i arrayen...
+    {
+        
+        objectArr.name.push(persArr[i].name);
+        objectArr.age.push(persArr[i].age);
+        
+    }
 
+    
+
+    return objectArr;
 }
+var data = [{ name: "John Häggerud", age: 37 }, { name: "Mats Loock", age: 46 }, { name: "Johan Leitet", age: 36 }];
 
+var result = makePerson(data);
+
+console.log(result);
