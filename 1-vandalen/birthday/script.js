6624f35aa5ccec0d34c0f2250ae891613de3d228
lr222gw <lr222gw@student.lnu.se>
2013-11-22 11:09:45 +0100

    Okej nu fungerar allt MEN ska rensa...

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -5,25 +5,47 @@ window.onload = function(){
 	
 	var birthday = function(date){
 		
-	    var birthday = new Date(date);	 
+	    var birthday = new Date(date + "T23:59:59"); // ändrar tiden på dygnet
+
+	    //var now = new Date();
+
+	    //var dayBirthday = ((((birthday.getTime() - now) / 1000) / 60) / 60) / 24; // Räknar ut antalet dagar mellan Bday och Idag.
+	    //dayBirthday = Math.floor(dayBirthday);
+
+	    //var birthday = dayBirthday - 1;
+
+	    //return dayBirthday;
+
+
 	    var now = new Date();
 
 	    var birthdayYear = birthday.getFullYear();
 	    var nowYear = now.getFullYear();
+	    birthday.setFullYear(nowYear)
+        if (birthday.getTime() - now.getTime() < 0)
+        {
+            birthday.setFullYear(nowYear + 1)
+        }
 
-	    var yearsBetweenBdayAndNow = nowYear - birthdayYear;
-	    var yearsInDaysBetweenBdayAndNow =                ; //(yearsBetweenBdayAndNow * 365) + 365; 
+        
+
+	    var yearsInDaysBetweenBdayAndNow = ((((now.getTime() - birthday.getTime()) / 1000) / 60) / 60) / 24; // Räknar ut antalet dagar från det datum man fyllt år tills idag..
+	    yearsInDaysBetweenBdayAndNow = Math.floor(yearsInDaysBetweenBdayAndNow);
+
+	    // var yearsInDaysBetweenBdayAndNow = ((((birthday.getTime() - now.getTime())/1000)/60)/60)/24; //(yearsBetweenBdayAndNow * 364) + 364; 
        
 
-	    var daysInMonthsUntilBday = ((((birthday.getTime() - now.getTime())/1000)/60)/60)/24; // Dividera med 1000 för att få millisekunder till sekunder.. /60 för att få det till minuter.. /60 igen för h... /24 för dagar..
-	    daysInMonthsUntilBday = Math.floor(daysInMonthsUntilBday) + 1; // + 1 gör så att allting fungerar! resten är avrundning..
-	    if (daysInMonthsUntilBday < 0) // Denna sats ser till att om man testar 
+	    var daysUntilBday = ((((birthday.getTime() - now.getTime())/1000)/60)/60)/24; // Dividera med 1000 för att få millisekunder till sekunder.. /60 för att få det till minuter.. /60 igen för h... /24 för dagar..
+	    daysUntilBday = Math.floor(daysUntilBday) ; // + 1 gör så att allting fungerar! resten är avrundning..
+	    
+	    if (daysUntilBday < 0) // Denna sats ser till att om man testar 
 	    {
-	        daysInMonthsUntilBday = yearsInDaysBetweenBdayAndNow + daysInMonthsUntilBday;
+            // 
+	        daysUntilBday = yearsInDaysBetweenBdayAndNow + daysUntilBday;//+ daysUntilBday
 	    }
 
 	    //return birthday.getDate();
-	    return daysInMonthsUntilBday;
+	    return daysUntilBday;
 			
 
 
