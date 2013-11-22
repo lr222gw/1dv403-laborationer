ec7a627c93199974d426e97f362f2c9c6a6e2332
lr222gw <lr222gw@student.lnu.se>
2013-11-22 12:26:23 +0100

    Labb 1.3 klar!
    Äntligen är jag klar med denna labb.

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -3,48 +3,29 @@
 window.onload = function(){
 
 	
-	var birthday = function(date){
-		
-	    var birthday = new Date(date + "T23:59:59"); // ändrar tiden på dygnet
-
-	    //var now = new Date();
-
-	    //var dayBirthday = ((((birthday.getTime() - now) / 1000) / 60) / 60) / 24; // Räknar ut antalet dagar mellan Bday och Idag.
-	    //dayBirthday = Math.floor(dayBirthday);
-
-	    //var birthday = dayBirthday - 1;
-
-	    //return dayBirthday;
-
-
-	    var now = new Date();
-
-	    var birthdayYear = birthday.getFullYear();
-	    var nowYear = now.getFullYear();
-	    birthday.setFullYear(nowYear)
-        if (birthday.getTime() - now.getTime() < 0)
-        {
-            birthday.setFullYear(nowYear + 1)
-        }
-
+    var birthday = function (date) {
+        var Y, M, D;
         
+        
+        if (!(Date.parse(date))) // Om Date.pase(date) blir null så kommer denna sats köras. Date.parse returnerar null om argumentsträngen är ogiltig..
+        {
+            throw new TypeError("Du måste ange ett giltigt datum.. i formen ÅÅÅÅ-MM-DD. ex: 1994-03-24");
+        };
 
-	    var yearsInDaysBetweenBdayAndNow = ((((now.getTime() - birthday.getTime()) / 1000) / 60) / 60) / 24; // Räknar ut antalet dagar från det datum man fyllt år tills idag..
-	    yearsInDaysBetweenBdayAndNow = Math.floor(yearsInDaysBetweenBdayAndNow);
+	    var birthday = new Date(date + "T23:59:59");//"T23:59:59" ändrar tiden på dygnet till sista sekunden.. :birthday = sätter datumet till datumet som användaren matar in.
+	    var now = new Date();                       // :now = sätter datumet som är dagens datum
+	    var nowYear = now.getFullYear();            // :nowYear = Tar reda på det fulla året av dagens år (genom now som vet dagens datum)
 
-	    // var yearsInDaysBetweenBdayAndNow = ((((birthday.getTime() - now.getTime())/1000)/60)/60)/24; //(yearsBetweenBdayAndNow * 364) + 364; 
-       
+	    birthday.setFullYear(nowYear)               // Ändrar året man har sagt att man fyller år till årets år, detta för att räkna år är ovesentligt när man räknar antalet dagar till nästa dag man fyller år.
+
+	    if (birthday.getTime() - now.getTime() < 0) // Denna if-sats görs om tiden som man fyller år på (från 1970) subbtraherat med tiden för dagens datum (från 1970) blir mindre än 0. <-- alltså om altalet millisekunder blir negativt.
+	    {
+	        birthday.setFullYear(nowYear + 1)       // Tar födelsedagens år och ändrar den till årets år +1. <-- gör att tiden aldrig kan bli negativ eftersom att man ändrar året på birthday när det blir det!
+	    };      
 
 	    var daysUntilBday = ((((birthday.getTime() - now.getTime())/1000)/60)/60)/24; // Dividera med 1000 för att få millisekunder till sekunder.. /60 för att få det till minuter.. /60 igen för h... /24 för dagar..
-	    daysUntilBday = Math.floor(daysUntilBday) ; // + 1 gör så att allting fungerar! resten är avrundning..
+	    daysUntilBday = Math.floor(daysUntilBday); //  avrundning..
 	    
-	    if (daysUntilBday < 0) // Denna sats ser till att om man testar 
-	    {
-            // 
-	        daysUntilBday = yearsInDaysBetweenBdayAndNow + daysUntilBday;//+ daysUntilBday
-	    }
-
-	    //return birthday.getDate();
 	    return daysUntilBday;
 			
 
