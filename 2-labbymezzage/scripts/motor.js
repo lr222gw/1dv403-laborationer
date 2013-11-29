c81a5815a4026f07a6e2a3e4a5bf9419ebc1fe03
lr222gw <lr222gw@student.lnu.se>
2013-11-29 17:24:27 +0100

    tog ner knappen i JS

--- 2-labbymezzage/scripts/motor.js
+++ 2-labbymezzage/scripts/motor.js
@@ -1,20 +1,33 @@
 "use strict";
 
 var motor = {
-	messages : [],
 	
-	functionToDo : function(){
-		var test = [];
-		test[0] = new Message("hej däär!", new Date()) // skapar ett meddelande object!
-		test[1] = new Message("din jäkel", new Date()) // skapar ett till meddelande object!
+	theSendFunction : function() {
+		var sendButton = document.getElementById("sendButton");  // hämtar ner sendButton från DOMen och lägger den i en variabel med samma namn...
 		
-		for(var i = 0; i < test.length ; i++)
-		{
-			motor.messages.push(test[i]); // Pushar in det i arrayen messages som ligger i objectet motor
+		sendButton.onclick = function(){
+			console.log("dude, it works!");
 		}
-		
-		alert(motor.messages[1]); // visar meddelandet från "brevet"
-	},
+	}
+
+
 }
 
-window.onload = motor.functionToDo;
+
+window.onload = motor.theSendFunction;
+
+
+//	messages : [],
+//	
+//	functionToDo : function(){
+//		var test = [];
+//		test[0] = new Message("hej däär!", new Date()) // skapar ett meddelande object!
+//		test[1] = new Message("din jäkel", new Date()) // skapar ett till meddelande object!
+//		
+//		for(var i = 0; i < test.length ; i++)
+//		{
+//			motor.messages.push(test[i]); // Pushar in det i arrayen messages som ligger i objectet motor
+//		}
+//		
+//		alert(motor.messages[1]); // visar meddelandet från "brevet"
+//		}
