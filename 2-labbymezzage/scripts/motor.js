599669622972d19257d9f8e95ce0729162a12e9c
lr222gw <lr222gw@student.lnu.se>
2013-11-29 18:04:11 +0100

    Stött på problem..
    Mina object som jag skapar med Message konstruktorn vill inte fungera...

--- 2-labbymezzage/scripts/motor.js
+++ 2-labbymezzage/scripts/motor.js
@@ -2,11 +2,24 @@
 
 var motor = {
 	
+	messageHolder : [],
+	
 	theSendFunction : function() {
 		var sendButton = document.getElementById("sendButton");  // hämtar ner sendButton från DOMen och lägger den i en variabel med samma namn...
+		var messageContent;
+		var message;
 		
 		sendButton.onclick = function(){
-			console.log("dude, it works!");
+			console.log("dude, it works!"); //ska ta bort sen
+			messageContent = document.querySelector("#messageContainer textarea").value; // hämtar ner innehållet från meddelandet och lägger den i en variabel "messageContent"
+			console.log(messageContent);//ska ta bort sen
+			
+			
+			message = new Message(messageContent, new Date()); // Skapar ett nytt MessageObject med innehållet från brevet och datumet som är nu!
+			motor.messageHolder.push(message); // Puttar in mitt nya object i en array!
+			console.log(message);
+			
+			
 		}
 	}
 
