7e1b504a312d2403714ed4da66ae44c3b3f67d27
lr222gw <lr222gw@student.lnu.se>
2013-12-03 20:00:53 +0100

    Bytte ut Id mot klasser...

--- 2-labbymezzage/scripts/motor.js
+++ 2-labbymezzage/scripts/motor.js
@@ -20,54 +20,59 @@ var motor = {
 			messageContent = document.querySelector("#messageContainer textarea").value; // hämtar ner innehållet från meddelandet och lägger den i en variabel "messageContent"
 			
 			console.log(messageContent);//ska ta bort sen
-			
+
 			
 			message = new Message(messageContent, new Date()); // Skapar ett nytt MessageObject med innehållet från brevet och datumet som är nu!
 			motor.messageHolder.push(message); // Puttar in mitt nya object i en array!
 			console.log(message);//ska ta bort sen
 			
+			document.querySelector("#messageContainer textarea").value = ""; // Tömmer textarean så att nästa meddelande man ska skicka kan börjas skriva på utan att ta bort det gamla meddelandet..
 			
-			
-			var prevMessage = document.getElementById("prevMessage"); //hämtar ner containern där mina meddelanden ska ligga			
-			
-			var prevTextBox = document.createElement("div"); //skapar en div där min text ska ligga i :)
-			prevTextBox.setAttribute("id", "prevTextBox"); //ger den ett ID			
-			prevTextBox.appendChild(document.createTextNode(message.getTheMessage())); // skickar med meddelandet..
-			
-			var prevDateBox = document.createElement("div");
-			prevDateBox.setAttribute("id", "prevDateBox");
-			prevDateBox.appendChild(document.createTextNode(message.getTheDate()));
-			
-			var soloMessage = document.createElement("div");
-			soloMessage.setAttribute("id", "soloMessage");
-			
-			var removeB = document.createElement("input"); // Skapar en knapp för att ta bort
-			removeB.setAttribute("id", "removeB");
-			removeB.setAttribute("type", "image");
-			removeB.setAttribute("value", "Ta bort");
-			removeB.setAttribute("src", "remove.png");
-			
-			var timeB = document.createElement("input"); // Skapar en knapp för att kolla exakt klockslag
-			timeB.setAttribute("id", "timeB");
-			timeB.setAttribute("type", "image");
-			timeB.setAttribute("value", "Kolla tidsstämpeln");
-			timeB.setAttribute("src", "time.png");
+			motor.renderMessages(message); // anropar metoden som skriver ut meddelandet, skickar med meddelandet skrivits.
 
-			
-
-			
-			soloMessage.appendChild(prevTextBox);
-			soloMessage.appendChild(prevDateBox);
-			soloMessage.appendChild(removeB);
-			soloMessage.appendChild(timeB);
-			
-			prevMessage.appendChild(soloMessage); // Skriver ut meddelandet..
 
 			
 			 mCount.innerHTML = "Antal meddelanden: " + motor.messageHolder.length; //Skriver ut antalet meddelanden som finns i messageHolder. = skriver ut antalet meddelanden som skapats..
 			
 
 		}
+
+	},
+	
+	renderMessages : function(message){	
+		
+		var prevMessage = document.getElementById("prevMessage"); //hämtar ner containern där mina meddelanden ska ligga			
+	
+		var prevTextBox = document.createElement("div"); //skapar en div där min text ska ligga i :)
+		prevTextBox.setAttribute("class", "prevTextBox"); //ger den ett ID			
+		prevTextBox.appendChild(document.createTextNode(message.getTheMessage())); // skickar med meddelandet..
+		
+		var prevDateBox = document.createElement("div");
+		prevDateBox.setAttribute("class", "prevDateBox");
+		prevDateBox.appendChild(document.createTextNode(message.getTheDate()));
+		
+		var soloMessage = document.createElement("div");
+		soloMessage.setAttribute("class", "soloMessage");
+	
+		var removeB = document.createElement("input"); // Skapar en knapp för att ta bort
+		removeB.setAttribute("class", "removeB");
+		removeB.setAttribute("type", "image");
+		removeB.setAttribute("value", "Ta bort");
+		removeB.setAttribute("src", "remove.png");
+		
+		var timeB = document.createElement("input"); // Skapar en knapp för att kolla exakt klockslag/datum
+		timeB.setAttribute("class", "timeB");
+		timeB.setAttribute("type", "image");
+		timeB.setAttribute("value", "Kolla tidsstämpeln");
+		timeB.setAttribute("src", "time.png");
+		
+		soloMessage.appendChild(prevTextBox);
+		soloMessage.appendChild(prevDateBox);
+		soloMessage.appendChild(removeB);
+		soloMessage.appendChild(timeB);
+		
+		prevMessage.appendChild(soloMessage); // Skriver ut meddelandet..
+		
 	}
 
 
