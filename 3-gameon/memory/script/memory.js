3cab923cb58b1f2834deaa9d93dc9ac191296d32
lr222gw <lr222gw@student.lnu.se>
2013-12-13 10:16:55 +0100

    Revert "ny"
    This reverts commit c64ce76acb23f4baaac2eeed04ed7a5daa64b8b4.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -4,16 +4,15 @@ var memory = {
 	cardArray : [],
 	randomArr : [], 
 	chosenBricks : [],
-	attempts : 0,
-	statusCheckListSummary : 0,
+	attempts : 0, 
 	startFunction : function(){
 		
 		
 		
 		
 		memory.MemoryObj(8); // Antal par
-		memory.randomPlacement(); // bygger upp en array för att hitta platserna åt alla kort..
-		memory.placeCards(4, 4); // här kan man ändra raderna..-++
+		memory.randomPlacement();
+		memory.placeCards(4, 4); // här kan man ändra raderna..
 		
 		
 		
@@ -38,7 +37,7 @@ var memory = {
 				//}
 				card = document.createElement("a");
 				
-				card.setAttribute("coupleId", "pics/"+i+".png");
+				card.setAttribute("coupleId", i);
 				card.setAttribute("CardId", j);
 				card.setAttribute("status", 0);
 				card.setAttribute("href", "#");							
@@ -46,9 +45,9 @@ var memory = {
 				img = document.createElement("img");
 				img.setAttribute("src", "pics/fast/ask.png");
 				
-				//realImg = document.createElement("div");
-				//realImg.setAttribute("src", "pics/"+i+".png");
-				//img.appendChild(realImg);
+				realImg = document.createElement("div");
+				realImg.setAttribute("src", "pics/"+i+".png");
+				img.appendChild(realImg);
 				
 				
 				
@@ -83,8 +82,10 @@ var memory = {
 	},
 	
 	placeCards : function(rows, cols){
-		var i, j, k, l, aTag, thisBrick, x, row, questionmark, setAttribute, winChecker, winCheckerArr; 
+		var i, j, k, l, aTag, thisBrick, x, row, questionmark, statusCheckList, statusCheckListSummary, setAttribute, winChecker, winCheckerArr; 
 		x = 0;
+		statusCheckListSummary = 0;
+		statusCheckList = [];
 		winCheckerArr = [];
 		for(i = 0; i < rows; i += 1){
 			
@@ -101,100 +102,87 @@ var memory = {
 				
 				thisBrick.onclick = function(e){
 					//e.target.remove();
+					var resetter = function(){
+						
+						for(k = 0; k < 2; k +=1){// for loop för att kolla om aTag har mer än 2 st status som har värdet 1.
+						
+							// ↓ dålig lösning då detta resettar ALLA brickor..
+							//document.getElementById("box").getElementsByTagName("a")[k].firstChild.setAttribute("src", questionmark);
+							
+							
+							memory.chosenBricks[k].setAttribute("src", questionmark); // tar bort de brickorna som man valt, specifikt!
+							
+						}
+						
+						for(k = 0; k < 2; k +=1){ // Denna kod utförs när resetter functionen anropas. den skulle utföras i
+
+							memory.chosenBricks[k].parentNode.setAttribute("status", 0); // sätter "status" till 0
+
+						}
+						
+						//memory.chosenBricks[k].parentNode.setAttribute("status", 0); 
+						memory.chosenBricks = []; // rensar arrayen för nytt bruk..
+						statusCheckListSummary = 0; // måste sättas till noll annats fortsätter metoden att anropa resetter och då blir det knas!					
+						
+					};
 					
-					if(memory.statusCheckListSummary >= 2 || e.target === memory.chosenBricks[0]){
-						return;
-					}
 					questionmark = e.target.getAttribute("src"); //sparar ner vägen till "?"-tecknet..
 					
 					memory.chosenBricks.push(e.target);//  lägga de två valda brickorna i en array som rensas varje gång brickorna tas bort..
 					
-					e.target.setAttribute("src", e.target.parentNode.getAttribute("coupleId"));//sätter attributet för sökvägen (src) till sökvägen som leder till bilden som hör till kortet.. 
+					e.target.setAttribute("src", e.target.firstChild.getAttribute("src"));//sätter attributet för sökvägen (src) till sökvägen som leder till bilden som hör till kortet.. 
 					
-					if(e.target.getAttribute("src") ===  e.target.parentNode.getAttribute("coupleId")){ // om  sökvägen är samma i källan som visas.
+					if(e.target.getAttribute("src") ===  e.target.firstChild.getAttribute("src")){ // om  sökvägen är samma i källan som visas.
 						
 						e.target.parentNode.setAttribute("status", 1); // sätt status till 1
 					}
 					
-					memory.getStatusCheckListSummary(); // anropar metoden som tar fram statusCheckList, alltså hur många kort som är uppvända
+					aTag = document.getElementById("box").getElementsByTagName("a");
+					for(k = 0; k < aTag.length; k +=1){// for loop för att kolla om aTag har mer än 2 st status som har värdet 1.
+						
+						statusCheckList.push(parseInt(aTag[k].getAttribute("status"))); // tar ner alla status och parsar om från string till int..
+						
+						statusCheckListSummary += statusCheckList[k]; //  kollar det totala värdet av arrayen. Om det är över 2 då är 2 kort uppe...
+						
+					}
 					
-					memory.statusChecker(questionmark);
+					if(statusCheckListSummary >= 2 ){ // om 2 kort är uppe...
+						
+						if(memory.chosenBricks[0].parentNode.getAttribute("coupleid") === memory.chosenBricks[1].parentNode.getAttribute("coupleid")){ 
+							memory.chosenBricks = []; // rensar så att det är fritt fram för nästa par..
+							statusCheckListSummary = 0; 
+							
+							winCheckerArr = []; // tommer.. 
+							winChecker = document.getElementsByTagName("a");
+							for(l = 0; l < winChecker.length; l +=1){
+								winCheckerArr.push(parseInt(winChecker[l].getAttribute("status"))); // gör om listan till en array så att jag kan köra indexOf på den. parsar om från string till number.. 
+							}
+							if(winCheckerArr.indexOf(0) === -1){ // kontrollerar om alla kort är uppvända! 
+								alert("DU klarade spelet! Du klarade det på "+memory.attempts+" Försök!");
+							}
+							
+						}else{
+							setTimeout(resetter, 500); // efter en sekund så rensas fönstret..
+							memory.attempts += 1;
+						}
+						
+						
+						
+					}
+
 					
 				};
-
+				
+				
 				x += 1; // denna nollställs bara efter funktionen har körts.. den är lämplig att använda för att J skulle bara nollställas och "förstöra" allt...
 				
-			}
-						
-			
-		}
-
-		
-		
-	},
-	resetter : function(questionmark){
-				var k;
-		for(k = 0; k < 2; k +=1){// for loop för att kolla om aTag har mer än 2 st status som har värdet 1.
-		
-			// ↓ dålig lösning då detta resettar ALLA brickor..
-			//document.getElementById("box").getElementsByTagName("a")[k].firstChild.setAttribute("src", questionmark);
-			
-			
-			memory.chosenBricks[k].setAttribute("src", questionmark); // tar bort de brickorna som man valt, specifikt!
+			}			
 			
 		}
 		
-		for(k = 0; k < 2; k +=1){ // Denna kod utförs när resetter functionen anropas. den skulle utföras i
-
-			memory.chosenBricks[k].parentNode.setAttribute("status", 0); // sätter "status" till 0
-
-		}
-		
-		//memory.chosenBricks[k].parentNode.setAttribute("status", 0); 
-		memory.chosenBricks = []; // rensar arrayen för nytt bruk..
-		memory.statusCheckListSummary = 0; // måste sättas till noll annars fortsätter metoden att anropa resetter och då blir det knas!					
-		
-	},
-	
-	statusChecker : function(questionmark){
-		var winCheckerArr, winChecker, l;
-		if(memory.statusCheckListSummary >= 2 ){ // om 2 kort är uppe...
-						
-			if(memory.chosenBricks[0].parentNode.getAttribute("coupleid") === memory.chosenBricks[1].parentNode.getAttribute("coupleid")){ 
-				memory.chosenBricks = []; // rensar så att det är fritt fram för nästa par..
-				memory.statusCheckListSummary = 0; 
-				
-				winCheckerArr = []; // tommer.. 
-				winChecker = document.getElementsByTagName("a");
-				for(l = 0; l < winChecker.length; l +=1){
-					winCheckerArr.push(parseInt(winChecker[l].getAttribute("status"))); // gör om listan till en array så att jag kan köra indexOf på den. parsar om från string till number.. 
-				}
-				if(winCheckerArr.indexOf(0) === -1){ // kontrollerar om alla kort är uppvända! 
-					alert("DU klarade spelet! Du klarade det på "+memory.attempts+" Försök!");
-				}
-				
-			}else{
-				setTimeout(function(){memory.resetter(questionmark)}, 1000); // efter en sekund så rensas fönstret med resetter()..
-								//   ^att sätta functionen till att kontakta funktionen görs att memory.resetter funktionen ej kontaktas förens om 1 sek.. 
-				memory.attempts += 1;
-			}
-		}
-	},
-	
-	getStatusCheckListSummary : function(){	
-		var k, statusCheckList, aTag;
-		statusCheckList = [];
-		
-		aTag = document.getElementById("box").getElementsByTagName("a");
-		for(k = 0; k < aTag.length; k +=1){// for loop för att kolla om aTag har mer än 2 st status som har värdet 1.
-			
-			statusCheckList.push(parseInt(aTag[k].getAttribute("status"))); // tar ner alla status och parsar om från string till int..
-			
-			memory.statusCheckListSummary += statusCheckList[k]; //  kollar det totala värdet av arrayen. Om det är över 2 då är 2 kort uppe...
-			
-		}
 		
 	}
+	
 	
 	
 	
