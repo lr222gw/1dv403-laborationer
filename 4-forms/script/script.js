884cad9c4cf2ccc14a2bb06501117eeac69a9904
lr222gw <lr222gw@student.lnu.se>
2013-12-17 22:59:52 +0100

    Läst in  Inputrutorna!

--- 4-forms/script/script.js
+++ 4-forms/script/script.js
@@ -2,9 +2,20 @@
 var validator = {
 	
 	check : function(){
+		var emme, i;
+		emme = [];
+		emme = document.getElementsByTagName("input"); // Hämtar ner alla Inputs till en lista
 		
 		
-		alert();
+		for( i = 0; i < emme.length ; i += 1){ // Genom att sätta min  funktion i en loop så hittar jag snabbt vilken "input" jag tryckt på.
+			
+			emme[i].onblur = function(e){
+				
+				alert(e.target);
+				e.target.blur(); // tar bort fokus från fältet som är fel.
+				
+			};
+		}
 		
 	}
 	
