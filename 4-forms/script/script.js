4dfd8d224bf6b597acda5dfac3a07a3e2905ddc1
lr222gw <lr222gw@student.lnu.se>
2013-12-18 00:46:05 +0100

    gjort funktion, regEx för mail. Ej klart..

--- 4-forms/script/script.js
+++ 4-forms/script/script.js
@@ -2,26 +2,88 @@
 var validator = {
 	
 	check : function(){
-		var emme, i;
+		var emme, i, targetId, targetContent;
 		emme = [];
 		emme = document.getElementsByTagName("input"); // Hämtar ner alla Inputs till en lista
 		
 		
 		for( i = 0; i < emme.length ; i += 1){ // Genom att sätta min  funktion i en loop så hittar jag snabbt vilken "input" jag tryckt på.
 			
-			emme[i].onblur = function(e){
+			emme[i].onblur = function(e){								
 				
-				alert(e.target);
+				targetContent = e.target.value;
+				
+				targetId = e.target.id; // tar reda på vilken input man har tryckt ifrån (onblur)
+				
+				switch(targetId){ // Anger olika beskrivningar beroende på vilken input/ruta som har 
+					
+					case "mailId":
+					// target id contet ska skickas till checkamil där den checkas med regex
+					validator.checkMail(targetContent);
+					break;
+					
+					case "PostnummerId":
+					
+					break;
+					default:
+					
+					break;
+				}
+				
 				e.target.blur(); // tar bort fokus från fältet som är fel.
 				
 			};
 		}
 		
-	}
+	},
 	
+	checkMail : function(targetContent){
+		var regExMail, result;	// http://gskinner.com/RegExr/ sida för att testa regulära uttryck, användbart..
+		
+		
+		if(targetContent === ""){ // Om targetContent är "" så ska ett meddelande lämnas att fältet är tomt..
+			return;
+		} 
+		
+		regExMail = /[^\.].+[^\.^\\]+@(\S.+\.(?!\.).+(?!\.))+/gi; // använder "[^\.].+[^\.^\\]+@(\S.+\.(?!\.).+(?!\.))+" för att man kan skriva epostadresser på så många konstiga sätt. och den här klarar allt så länge du har något före och efter @ och sen en punkt och "com"/"se" etc..
+		
+		result = targetContent.match(regExMail); // om Null returneras så är epostadressen ogiltig
+	}
 	
 	
 	
 }
 
 window.onload = validator.check;
+
+
+/*
+ 
+ * Lista på Mail som ej fungerar, samt mail som fungerar...
+ * 
+FUNGERAR:
+email@example.com
+firstname.lastname@example.com
+email@subdomain.example.com
+firstname+lastname@example.com
+email@123.123.123.123
+email@[123.123.123.123]
+"email"@example.com
+1234567890@example.com
+email@example-one.com
+_______@example.com
+email@example.name
+email@example.museum
+email@example.co.jp
+firstname-lastname@example.com
+f@gmail.se
+
+FUNGERAR INTE: (undantag = * )
+me@				*
+@example.com	*
+me.@example.com
+.me@example.com	*
+me@example..com	*
+me.example@com
+me\@example.com
+ * */
