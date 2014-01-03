6a023190b911a35b18b7936255eb1b261aee5807
lr222gw <lr222gw@student.lnu.se>
2014-01-03 21:16:36 +0100

    Gjort en grund till min konstrukotr funktion!

--- 5-pwd/script/script.js
+++ 5-pwd/script/script.js
@@ -3,8 +3,40 @@
 var DESKTOPAPP = {
 	
 	master : function(){ // Huvudfunktionen som kör allt..
+		var he = new DESKTOPAPP.konstructors.DeskButtonFunctionHolder("hehe", "heh", "hoho");
+	}, 
+	
+	konstructors : { // Objekt där jag lagrar mina konstruktorer...
 		
-	} 
+		//Konstruktor för knapp/icon... här förvarar jag konstruktorn för knappskapandet samt prototyperna..
+		DeskButtonFunctionHolder : function(name, buttonFunc, icon){ 
+			var theButtonToReturn;
+			
+			function DeskButton(name, buttonFunc, icon){
+				this.name = name; //namnet på iconen
+				this.buttonFunc = buttonFunc; // Metoden som ska kontaktas när denna knapp trycks
+				this.icon = icon; // En bild till knappen
+			}			
+				
+			DeskButton.prototype.doAction = function(){
+				alert();
+			};
+			
+			theButtonToReturn = new DeskButton(name, buttonFunc, icon); // skapar en knapp som returneras...
+			return theButtonToReturn;
+		},
+			
+			//Konstruktor för mina fönster..
+			DeskWindow : function(){
+				
+			} 
+				
+		},
+	
+	myFunctions : { // Object där jag lagrar mina funktioner, mm...
+		
+	}
+	
 	
 };
 
