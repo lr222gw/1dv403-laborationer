af348dc5bd9dc510fdd737d6c9338a5a6e6c2b22
lr222gw <lr222gw@student.lnu.se>
2014-01-03 21:30:23 +0100

    Jobbar på konstruktorn till DeskWindow

--- 5-pwd/script/script.js
+++ 5-pwd/script/script.js
@@ -3,18 +3,19 @@
 var DESKTOPAPP = {
 	
 	master : function(){ // Huvudfunktionen som kör allt..
-		var he = new DESKTOPAPP.konstructors.DeskButtonFunctionHolder("hehe", "heh", "hoho");
+		var he = new DESKTOPAPP.konstructors.deskButtonFunctionHolder("hehe", "heh", "hoho");
+		var ho = new DESKTOPAPP.konstructors.DeskWindowFunctionHolder("aWindow", "picstuff");
 	}, 
 	
 	konstructors : { // Objekt där jag lagrar mina konstruktorer...
 		
 		//Konstruktor för knapp/icon... här förvarar jag konstruktorn för knappskapandet samt prototyperna..
-		DeskButtonFunctionHolder : function(name, buttonFunc, icon){ 
+		deskButtonFunctionHolder : function(name, buttonFunc, icon){ 
 			var theButtonToReturn;
 			
 			function DeskButton(name, buttonFunc, icon){
 				this.name = name; //namnet på iconen
-				this.buttonFunc = buttonFunc; // Metoden som ska kontaktas när denna knapp trycks
+				this.buttonFunc = buttonFunc; // Metoden som ska kontaktas när denna knapp trycks (tex vad innehållet ska vara i fönster ruta)
 				this.icon = icon; // En bild till knappen
 			}			
 				
@@ -22,14 +23,27 @@ var DESKTOPAPP = {
 				alert();
 			};
 			
-			theButtonToReturn = new DeskButton(name, buttonFunc, icon); // skapar en knapp som returneras...
+			theButtonToReturn = new DeskButton(name, buttonFunc, icon); // skapar en knapp med värderna som skickades in till deskButtonFunctionHolder som returneras...
 			return theButtonToReturn;
 		},
 			
 			//Konstruktor för mina fönster..
-			DeskWindow : function(){
+			DeskWindowFunctionHolder : function(name, icon){
+				var theWindowToReturn;
 				
-			} 
+				function DeskWindow(name, icon){
+					this.name = name;
+					this.icon = icon;
+				}
+				
+				DeskWindow.prototype.structure = function(){
+					
+				};
+				
+				theWindowToReturn = new DeskWindow(name, icon); // skapar en knapp med värderna som skickades in till deskButtonFunctionHolder som returneras...
+				return theWindowToReturn;
+			}
+			
 				
 		},
 	
